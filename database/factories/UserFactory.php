2262f124324bb09f6447bed16df1b75880f0795f
RyoFs <fahreziryo@gmail.com>
2026-01-03 11:16:41 +0700

    Fix UserFactory to use User model correctly

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -15,10 +16,12 @@ class UserFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password;
-
+    protected $model = User::class;
+    
     /**
      * Define the model's default state.
      *
+     
      * @return array<string, mixed>
      */
     public function definition(): array
