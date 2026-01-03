394d86704df90d89d581d772b20e6a54cc986bc2
RyoFs <fahreziryo@gmail.com>
2026-01-03 11:29:06 +0700

    Update user creation in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        \App\Models\User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => bcrypt('12345678'),
